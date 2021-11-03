9c4705149837f5352a5c160911bb2ff3cd5164a8
Ines Khemir <ikhemir@hotmail.fr>
2021-11-03 14:42:19 +0100

    15 [ADD][PACKAGE] create a package for the tree
    DONE:
        - Create a package for the decision tree.
        - Call it in the notebook (a jupyter notebook provides better visibility)
    
    TODO:
        - Review and merge
    
    issue number : #15

---
+++ DecisionTree/Node.py
@@ -0,0 +1,38 @@
+"""
+Node Class
+
+Node used in the decision tree
+"""
+
+class Node:
+    def __init__(self, left=None, right=None, information_gain=None,
+                 threshold=None, column_name=None, column_index=None,
+                 operator=None, value=None):
+        """
+        Attributes:
+            left (Node) : #TODO
+            right (Node) : #TODO
+
+            information_gain (float) : The information gain with this decision.
+            threshold (float) : The value to compare during the decision.
+            column_name (str) : The index of the decision column in the original data.
+            column_index (int) : Index of the decision column in the original data.
+            operator (str) : '=' if the decision is made on a binary value else '<='
+
+            value (int) : The most represented class by this node.
+        """
+
+
+        #children
+        self.left = left
+        self.right = right
+
+        #decision node
+        self.information_gain = information_gain
+        self.threshold = threshold
+        self.column_name = column_name
+        self.column_index = column_index
+        self.operator = operator
+
+        #leaf node
+        self.value = value
