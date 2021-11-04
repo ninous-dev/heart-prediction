ece6c5b381e11a893fc0039fc823097753803d45
Ines Khemir <ikhemir@hotmail.fr>
2021-11-04 15:20:18 +0100

    15 [FIX][DECISIONTREE] possible binary thresholds
    DONE:
        - Fix the possible_thresholds list for binary value : the value compare was 1 but we need to compare value inferior or equal to 0 and superior value.
    
    TODO:
        - Remove information_gain from pretty_print_rec
    
    issue number : #15

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -41,7 +41,6 @@ class DecisionTree:
         Returns:
             inputs (array), labels (array)
         """
-
         return dataset[:, :len(self.x_col_names)], dataset[:, len(self.x_col_names):]
 
     def build_tree(self, dataset, depth=0):
@@ -101,7 +100,7 @@ class DecisionTree:
 
             if len(possible_thresholds) == 2:
                 binary_col = True
-                possible_thresholds = possible_thresholds[1:]
+                possible_thresholds = possible_thresholds[:1]
             else:
                 possible_thresholds = possible_thresholds[1:-1]
 
@@ -119,8 +118,6 @@ class DecisionTree:
                     split['column_index'] = i
                     split['operator'] = "=" if binary_col else "<="
 
-                #if binary_col:
-                #    break
         return split
 
     def split(self, dataset, threshold, column_index, binary_col):
