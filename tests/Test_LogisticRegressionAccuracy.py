53a76f6902fb496d3f53ae562eb7081a0f159e48
Ines Khemir <ikhemir@hotmail.fr>
2021-11-07 18:03:55 +0100

    10 [ADD][TESTS] testsuite and save weights
    DONE:
        - Add tests.
        - Save accuracy for the testsuite.
    
    TODO:
        - Add mean accuracy compute.
        - Compare each loss function.
    
    Next step ideas:
        - Anova to visualize the dependence of each feature.
        - Multinomial logistic regression to predict multi-classes
        - Binning data : for exemple SYSBP could probably be grouped by values corresponding to normal, pre-hypertensive and hypertensive.
    
    issue number : #10

--- tests/Test_LogisticRegressionAccuracy.py
+++ tests/Test_LogisticRegressionAccuracy.py
@@ -6,12 +6,10 @@ import os
 
 from sklearn.model_selection import train_test_split
 
-from DecisionTree import DecisionTree
-
 #Load data
 data_path = "data/clean_data.csv"
-accuracies_file_path = "tests/tests_pkl/decision_tree/test_decisionTree_accuracies.pkl"
-actual_accuracies_file_path = "current_accuracies/decision_tree/actual_decisionTree_accuracies.pkl"
+accuracies_file_path = "tests/tests_pkl/logistic_regression/LogisticRegression_accuracies.pkl"
+actual_accuracies_file_path = "current_accuracies/logistic_regression/LogisticRegression_accuracies.pkl"
 
 data = np.loadtxt(data_path, delimiter=",",dtype=float, skiprows=1)
 col_names = np.genfromtxt(data_path , delimiter=',', names=True, dtype=float).dtype.names[1:31]
@@ -29,25 +27,7 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.4, random_s
 #Define the maximum depth
 DEPTH = 5
 
-class TestDecisionTreeClass:
-    def test_compute_accuracies(self):
-        total_accuracy = 0
-        accuracies = {}
-        for i, column in enumerate(y_col_names):
-            i = i + 23
-            Y = data[:, i:(i+1)]
-            X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.4, random_state=42)
-            decision_tree = DecisionTree(max_depth=DEPTH)
-            decision_tree.fit(X_train, Y_train, x_col_names=x_col_names)
-            accuracy = decision_tree.prediction_analyse(X_test, Y_test, False, False)
-            accuracies[column] = accuracy
-            total_accuracy += accuracy
-
-        accuracies_file = open(accuracies_file_path, "wb")
-        pickle.dump(accuracies, accuracies_file)
-        accuracies_file.close()
-
-        assert os.path.isfile(accuracies_file_path) == True
+class TestLogisticRegressionClass:
 
     def test_compare_accuracies(self):
         accuracies_file = open(accuracies_file_path, "rb")
@@ -62,14 +42,21 @@ class TestDecisionTreeClass:
 
         lower_accuracies = []
         no_lower = True
+        upper = False
 
+        print(actual_values, new_values)
         for key in keys:
             is_upper = actual_values[key] <= new_values[key]
+            comparaison = actual_values[key] < new_values[key]
 
             if not is_upper:
                 no_lower = False
+            if not comparaison:
+                upper = True
 
             comparaison_str = str(actual_values[key]) + " <= " + str(new_values[key])
             lower_accuracies.append(key + " : " + str(is_upper) + ' ' + comparaison_str)
 
         assert no_lower, "key comparaison (actual vs new):\n{}".format("\n".join(lower_accuracies))
+        assert upper, "Better accuracy please save weights and save accuracies before the commit.\nkey comparaison (actual vs new):\n{}".format("\n".join(lower_accuracies))
+
